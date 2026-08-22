a8b7898237cb787e8ca641ebdcca2bd92cd61fc1
omkarphadtare53-lgtm <omkarphadtare53@gmail.com>
2026-08-22 19:15:33 +0530

    Update backend message to 'ganesh backend is working!'

--- backhend/server.js
+++ backhend/server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 10000;
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "Ekopa backend is working!"
+    message: "ganesh backend is working!"
   });
 });
 
